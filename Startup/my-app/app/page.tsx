34c1cf08afe7a08a0be9512577852447b27d45dd
Demarcus Braclet <demarcusbraclet@outlook.com>
2024-09-06 12:07:02 -0500

    1. Basic home page

--- Startup/my-app/app/page.tsx
+++ Startup/my-app/app/page.tsx
@@ -1,7 +1,12 @@
 import Image from "next/image";
+import Link from 'next/link'
+ 
 export default function Home() {
   return (
     <div>
+       <li>
+        <Link href="/">Home</Link>
+      </li>
     <h1 style = {{color: 'white'}}>Planning Poker</h1>
     <button className = "button-43"> Join as Host</button>
     <button className = "button-43"> Join as User</button>
