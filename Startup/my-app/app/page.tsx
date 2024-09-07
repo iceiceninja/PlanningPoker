8f516ce6feda6ee841c5621f33c8f21738aa2c7b
Demarcus Braclet <demarcusbraclet@outlook.com>
2024-09-07 17:00:12 -0500

    2. Routes, add test route

--- Startup/my-app/app/page.tsx
+++ Startup/my-app/app/page.tsx
@@ -1,15 +1,19 @@
+
 import Image from "next/image";
 import Link from 'next/link'
- 
+import { useRouter } from 'next/navigation';
+
 export default function Home() {
+
+
   return (
     <div>
        <li>
-        <Link href="/">Home</Link>
+        <Link href="/routes">Home</Link>
       </li>
     <h1 style = {{color: 'white'}}>Planning Poker</h1>
-    <button className = "button-43"> Join as Host</button>
+    <button  className = "button-43"> Join as Host</button>
     <button className = "button-43"> Join as User</button>
     </div>
   )
-}
+};
