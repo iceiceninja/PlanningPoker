79e82971da313b8c6d9c46c48df6a331e820b0d6
Demarcus Braclet <demarcusbraclet@outlook.com>
2024-09-11 17:47:50 -0500

    3. Working on socket connections

--- Startup/my-app/app/page.tsx
+++ Startup/my-app/app/page.tsx
@@ -1,19 +1,32 @@
-
+"use client"
 import Image from "next/image";
 import Link from 'next/link'
 import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+import io from 'socket.io-client';
 
 export default function Home() {
+  useEffect(() => {
+    const socket = io();
 
+    socket.on('connect', () => {
+      console.log('Connected to WebSocket server');
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
 
   return (
     <div>
        <li>
-        <Link href="/routes">Home</Link>
+        <Link href="/user">Home</Link>
       </li>
     <h1 style = {{color: 'white'}}>Planning Poker</h1>
     <button  className = "button-43"> Join as Host</button>
     <button className = "button-43"> Join as User</button>
+    <input type="text" />
     </div>
   )
 };
