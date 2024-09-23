56f77959db1897b0fc4723fbc7fbefad9de6700a
Demarcus Braclet <demarcusbraclet@outlook.com>
2024-09-22 19:47:39 -0500

    test

--- Startup/my-app/app/page.tsx
+++ Startup/my-app/app/page.tsx
@@ -2,12 +2,17 @@
 import Image from "next/image";
 import Link from 'next/link'
 import { useRouter } from 'next/navigation';
-import { useEffect, useMemo } from 'react';
+import { useState, useEffect, SetStateAction } from 'react';
 import io from 'socket.io-client';
 
 export default function Home() {
+  const router = useRouter(); // Initialize the router
+  const [hostJoined, setHostJoined] = useState(false);  // State to hold the input text
+  const [formData, setFormData] = useState({ hostName: '', sessionTopic: '' });
+
+
   useEffect(() => {
-    const socket = io("192.168.1.92:3000");
+    const socket = io(":3000");
 
     socket.on('connect', () => {
       console.log('Connected to WebSocket server');
@@ -18,10 +23,30 @@ export default function Home() {
     };
   }, []);
 
+  const joinAsHost = (event : any) => {
+    event.preventDefault();  // Prevent the default GET request
+    console.log('Form submitted:', formData);
+    setFormData(formData);
+    console.log("hello world");
+    if (hostJoined) {
+      joinAsPlayer();
+    }
+
+    setHostJoined(true);
+    router.push('/host'); // Navigate to /host
+  };
+
+
+  const joinAsPlayer = () => {
+    router.push('/user'); // Navigate to /user
+  };
+
+
+
   return (
     <div>
     <h1 style = {{color: 'black'}}>Planning Poker</h1>
-    <form>
+    <form onSubmit={joinAsHost}>
     <label  className = "label-input" htmlFor="hostName">Host's Name</label>
 <input
   type="text"
