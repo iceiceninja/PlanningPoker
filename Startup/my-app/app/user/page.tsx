151bc4bb06be0af3e3de1bc991cf8795119a800e
Demarcus Braclet <demarcusbraclet@outlook.com>
2024-11-19 19:55:51 -0600

    Removed console.logs

--- Startup/my-app/app/user/page.tsx
+++ Startup/my-app/app/user/page.tsx
@@ -201,12 +201,10 @@ export default function Host() {
     })
 
     socket.on("return_all_information", (data) => {
-        console.log(data);
         var currentAverage = data.currentAverage;
         var isRoundOver = data.isRoundOver;
         var canChangeVote = data.changeVote;
 
-        console.log(canChangeVote)
 
         setChecked(canChangeVote);
 
