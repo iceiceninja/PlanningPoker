696af661b79a3f58c47e2d3d8a56d9d65f5c2291
Demarcus Braclet <demarcusbraclet@outlook.com>
2024-11-19 19:51:29 -0600

    Fixed a warning and a error where the fonts would not download from google

--- Startup/my-app/tailwind.config.js
+++ Startup/my-app/tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [  
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}',],
   theme: {
     extend: {},
   },
